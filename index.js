3b35379e64444f809843641c8dbe358c8871e84e
Joseph Autorino <staircasesoul@gmail.com>
2021-04-06 15:19:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ var resipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value 
+
+  return object 
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Oject.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}

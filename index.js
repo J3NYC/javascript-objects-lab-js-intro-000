3846307c583ac89dad2530c38851193acfc952a4
Joseph Autorino <staircasesoul@gmail.com>
2021-04-06 15:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  const newObj = Oject.assign({}, object)
+  const newObj = Object.assign({}, object)
 
   delete newObj[key]
 
